2e4b95a61d0f54ac1a2dfa8c92d6c5bfab9e09f3
lmaddalena <mdn.luca@gmail.com>
2015-08-31 11:50:52 +0200

    added comments

--- 03_FileIO/app.js
+++ 03_FileIO/app.js
@@ -1,8 +1,10 @@
 ﻿var fs = require('fs');
 
+// lettura sincrona
 var contents = fs.readFileSync('package.json').toString();
 console.log(contents);
 
+// lettura asincrona
 fs.readFile('package.json', function (err, buf) {
     console.log(buf.toString());
 });
